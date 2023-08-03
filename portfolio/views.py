e9b0dcc6fc9ba70ec4f3609f12a471e9b78bae0c
janeffer945 <janeffernjeri945@gmail.com>
2023-08-04 00:32:28 +0300

    created contact page and authenticated

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,18 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+   if request.method=="POST":
+    name=request.POST.get('name')
+    email=request.POST.get('email')
+    phoneNo=request.POST.get('num')
+    desc=request.POST.get('desc') 
+    print(name,email,phoneNo,desc )
+    messages.success()
+    return redirect('/contact')
+   
+
+
+
     return render(request, 'contact.html')
 
 def services(request):
