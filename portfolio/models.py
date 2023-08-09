bc73c7fbc9b4503ee71354a6d404d4444d6d6a06
janeffer945 <janeffernjeri945@gmail.com>
2023-08-09 12:40:58 +0300

    modified templates

--- portfolio/models.py
+++ portfolio/models.py
@@ -16,7 +16,7 @@ class Blogs(models.Model):
    authname=models.CharField(max_length=15)
    img=models.ImageField(upload_to='blog', blank=True,null=True )
    timestamp=models.DateTimeField(auto_now_add=True) 
-   links=models.URLField(null=True, blank=True) 
+   links=models.URLField(null=True,blank=True ) 
    def __str__(self):
       return self.title   
 
