f8ab5b3b9a985c1e28c3458c2436fbe95c767222
janeffer945 <janeffernjeri945@gmail.com>
2023-08-15 11:35:15 +0300

    final commit

--- portfolio/views.py
+++ portfolio/views.py
@@ -26,16 +26,10 @@ def contact(request):
 
         query = Contact(name=fname, email=femail, phonenumber=phoneno, description=desc)
         query.save()
-
-        from_email=settings.EMAIL_HOST_USER 
-        connection=mail.get_connection() 
-        # connection.open() 
-        email_message=mail.EmailMessage(f'Email is from{fname}',f'UserEmail: {femail} /nUserPhoneNumber : {phoneno}/n/n/n  QUERY:{desc}',from_email,
-        ['janeffernjeri945@gmail.com'], connection=connection) 
-        connection.send_messages([email_message])
-        connection.close()
         messages.info(request, "Thanks for reaching to us we will get back to you soon...")
 
+
+        
        
         return redirect('/contact')  # Redirect to the 'contact' view
 
--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ SECRET_KEY = 'django-insecure-tm1c@vn#i401k36w8ioe4sva*f%kx*+khd#y10o5hc9rplh9-)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
