eef941289288e90dc2cc6aa1228603f22d5a76a5
janeffer945 <janeffernjeri945@gmail.com>
2023-08-15 14:37:40 +0300

    deploying

--- project/settings.py
+++ project/settings.py
@@ -154,7 +154,9 @@ STATICFILES_DIRS =[
 ]
 
 MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')    
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')  
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
